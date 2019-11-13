b0f8a115b2b9110afbf07ef6b8946edfd57c4f51
imfizh <43548110+imfizh@users.noreply.github.com>
2019-11-13 20:48:52 +0000

    map

---
+++ Aint253 escape room/Assets/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController cc;
+    float xInput;
+    float zInput;
+    public float speed = 12f;
+    float gravity = -9.81f;
+    Vector3 velocity;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2.0f;
+        }
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * xInput + transform.forward * zInput;
+        cc.Move(move * speed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        cc.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ Aint253 escape room/Assets/scripts/mouse_Look.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouse_Look : MonoBehaviour
+{
+
+    public float mouse_Sens = 100.0f;
+    public Transform PlayerBody;
+    float xMouse;
+    float yMouse;
+    float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xMouse = Input.GetAxis("Mouse X") * mouse_Sens * Time.deltaTime;
+        yMouse = Input.GetAxis("Mouse Y") * mouse_Sens * Time.deltaTime;
+        xRotation -= yMouse;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        PlayerBody.Rotate(Vector3.up * xMouse);
+    }
+}

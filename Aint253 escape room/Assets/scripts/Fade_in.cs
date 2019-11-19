187164f7b2181bd8d123cf023da744a4bc5a92ac
imfizh <43548110+imfizh@users.noreply.github.com>
2019-11-19 16:17:54 +0000

    please

---
+++ Aint253 escape room/Assets/scripts/Fade_in.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade_in : MonoBehaviour
+{
+    public Image BlackImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BlackImage.canvasRenderer.SetAlpha(1.0f);
+        fadeIn();
+        Anims();
+    }
+
+    void fadeIn()
+    {
+        BlackImage.CrossFadeAlpha(0, 3, false);
+    }
+    void Anims()
+    {
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>().SetTrigger("test");
+        GameObject.Find("phone").GetComponent<Animator>().SetTrigger("phone_trigger");
+        //Destroy(GameObject.Find("phone"));
+    }
+}
---
+++ Aint253 escape room/Assets/scripts/Start_script.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Movement>().enabled = false;
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<mouse_Look>().enabled = false;
+    }
+
+   
+}
